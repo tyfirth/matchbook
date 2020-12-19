6015e72504666a309b53ba7575c1c212abd3f726
tyfirth <tyfirth@gmail.com>
2020-12-18 17:40:53 -0800

    update matches controller, matches views

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -14,20 +14,21 @@ class MatchesController < ApplicationController
     @match = Match.new(match_params)
     @match.save
 
-    redirect_to @match
+    redirect_to match_path(@match)
   end
 
   def show
     @match = Match.find(params[:id])
     @match_game = Game.find_by(id: @match.game_id)
+    #do I need players or match_players here?
   end
 
   def edit
-    @match = Match.find(params[:id])
+    @match = Match.find_by(id: params[:id])
   end
 
   def update
-    @match = Match.find(params[:id])
+    @match = Match.find_by(id: params[:id])
     @match.update(match_params)
     @match.save
 
@@ -35,13 +36,19 @@ class MatchesController < ApplicationController
   end
 
   def destroy
+    @match = Match.find_by(id: params[:id])
+
+    redirect_to matches_path
 
   end
 
   private
 
   def match_params
-    params.require(:match).permit(:title, :match_datetime, :winner, :score, :notes, :game_id, :players)
+    params.require(:match).permit(:players, :title, :match_datetime, :winner, :score, :notes, :game_id)
   end
+
+  #getting error unpermitted parameter "players" on new match form...
+  #need to add players_ids column to matches_players table?
 
 end

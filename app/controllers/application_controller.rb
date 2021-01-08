41307a7c60ff723a45b569ea6ff53033904a8ce4
tyfirth <tyfirth@gmail.com>
2021-01-08 13:23:56 -0800

    Running into InvalidAuthenticityTokens when logging out from viewing players#index. Added skip_before_action :verify_authenticity_token to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :logged_in?
 
+  skip_before_action :verify_authenticity_token
+
 def current_user
   @player = Player.find_by(id: session[:player_id])
 end
